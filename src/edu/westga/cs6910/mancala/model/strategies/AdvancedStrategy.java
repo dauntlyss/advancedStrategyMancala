1380507d6c8179f3638c319ff0b0279b263068f4
Alyssa Harris <ah00214@my.westga.edu>
2022-07-07 13:58:14 -0400

    Added more functionality to selectPit method

--- src/edu/westga/cs6910/mancala/model/strategies/AdvancedStrategy.java
+++ src/edu/westga/cs6910/mancala/model/strategies/AdvancedStrategy.java
@@ -10,8 +10,26 @@ public class AdvancedStrategy implements SelectStrategy {
 
 	@Override
 	public int selectPit(int[] theBoard) {
+		int closestPit = theBoard.length - 2;
+		if (theBoard[closestPit] == 1) {
+			return 6;
+		}
+		
+		if (theBoard[closestPit -1] == 2) {
+			return 5;
+		}
+		
+		return this.selectClosestPitWithStones(theBoard);
+	}
 
-		return -1;
+	private int selectClosestPitWithStones(int[] theBoard) {
+		int closestPit = theBoard.length - 2;
+		while (closestPit > theBoard.length / 2 - 1 && theBoard[closestPit] == 0)  {
+			if (theBoard[closestPit] == 0) {
+				closestPit--;
+			}
+		}
+		return closestPit;
 	}
 
 }
