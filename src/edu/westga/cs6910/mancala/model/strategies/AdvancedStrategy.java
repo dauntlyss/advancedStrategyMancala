7399534595e5c037537d86152f96d2ab5c98aaae
Alyssa Harris <ah00214@my.westga.edu>
2022-07-07 13:19:01 -0400

    Created advancedstrategy class

---
+++ src/edu/westga/cs6910/mancala/model/strategies/AdvancedStrategy.java
@@ -0,0 +1,17 @@
+package edu.westga.cs6910.mancala.model.strategies;
+
+/**
+ * Defines the computer player's advancedStrategy behaviors
+ *
+ * @author Alyssa Harris
+ * @version Jul 7, 2022
+ */
+public class AdvancedStrategy implements SelectStrategy {
+
+	@Override
+	public int selectPit(int[] theBoard) {
+
+		return -1;
+	}
+
+}
